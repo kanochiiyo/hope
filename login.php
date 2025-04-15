d07e79d9dc306a804fc393bbe7da5c4d862675f2
dini <andiniandarestaa@gmail.com>
2025-04-15 11:06:08 +0700

    create login page

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php' ?>
+
+<body>
+    <section id="login">
+        <div class="container-fluid d-flex justify-content-center align-items-center">
+            <div class="card p-4 shadow rounded-4 border-0" style="max-width: 400px; width: 100%;">
+                <div class="card-body">
+                    <h2 class="fw-bold text-center">LOGIN</h2>
+                    <p class="text-muted"> How to i get started lorem ipsum dolor at?</p>
+                    <form>
+                        <div class="mb-3 text-start">
+                            <input type="text" class="form-control" placeholder="Username">
+                        </div>
+                        <div class="mb-3 text-start">
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+                        <button type="submit"
+                            class="btn d-flex justify-content-center gradient-btn border-0 rounded-4 fw-semibold text-white"
+                            style="width: 124px; height: 52px">LOGIN</button>
+                    </form>
+
+                    <div class="text-muted text-center my-3">Login with Others</div>
+
+                    <button class="social-btn rounded-4 mb-2">
+                        <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google">
+                        Login with <strong>Google</strong>
+                    </button>
+                    <button class="social-btn rounded-4">
+                        <img src="https://www.svgrepo.com/show/475647/facebook-color.svg" alt="Facebook">
+                        Login with <strong>Facebook</strong>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    </section>
+</body>
+
+</html>
