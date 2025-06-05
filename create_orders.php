daa447cc750185f84f8bd9f2694403f73c08fdd8
kanochiiyo <124230084@student.upnyk.ac.id>
2025-06-06 00:41:05 +0700

    add payment page and update order form styles

--- create_orders.php
+++ create_orders.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-require_once(__DIR__ . "/functions/authentication.php");
-require_once(__DIR__ . "/functions/functions.php");
+require_once __DIR__ . "/functions/authentication.php";
+require_once __DIR__ . "/functions/functions.php";
 
 $connection = getConnection();
 
@@ -10,9 +10,72 @@ if (!isLogged()) {
     header("Location:login.php");
 }
 
-include(__DIR__ . "/templates/header.php");
-
+include __DIR__ . "/templates/header.php";
 ?>
+
 <div class="container-fluid d-flex" id="createOrder">
     <?php include(__DIR__ . "/templates/sidebar.php"); ?>
-</div>
+
+    <div class="order-form col-10" id="orderForm">
+        <div class="card m-5 p-3 border-0 shadow rounded-4" style="width: 61rem;">
+            <div class="card-body">
+                <div class="header d-flex justify-content-between">
+                    <h3 class="fw-bold">Create Orders</h3>
+                </div>
+                <div class="order-form">
+                    <form action="" method="post" class="border-0">
+                        <div class="mb-3 row">
+                            <label for="inputName" class="col-sm-3 col-form-label">Name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="inputName" name="custName"
+                                    placeholder="Input Name">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputPhone" class="col-sm-3 col-form-label">Phone Number</label>
+                            <div class="col-sm-9">
+                                <input type="number" class="form-control" id="inputPhone" name="phoneNumber"
+                                    placeholder="Input Phone Number">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputProjectName" class="col-sm-3 col-form-label">Project Name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="inputProjectName" name="projectName"
+                                    placeholder="Input Project Name">
+                            </div>
+                        </div>
+                        <!-- slect projct type -->
+                        <div class="mb-3 row">
+                            <label for="inputQuantity" class="col-sm-3 col-form-label">Quantity</label>
+                            <div class="col-sm-9">
+                                <input type="number" class="form-control" id="inputQuantity" name="quantity"
+                                    placeholder="Input Quantity">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputShippingAddres" class="col-sm-3 col-form-label">Shipping Address</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="inputShippingAddres" name="shippingAddress"
+                                    placeholder="Input Shpping Address">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="inputDesc" class="col-sm-3 col-form-label">Desciption</label>
+                            <div class="col-sm-9">
+                                <textarea class="form-control" placeholder="Description" id="inputDesc"
+                                    style="height: 100px"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row d-flex justify-content-between" style="margin: 0px 1px;">
+                            <input type="submit" class="back-btn col-2" value="Back">
+                            <input type="submit" class="submit-btn col-2" value="Submit">
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -17,17 +17,83 @@ include __DIR__ . "/templates/header.php";
   <?php include __DIR__ . "/templates/sidebar.php"; ?>
 
   <div class="main-content col-10" id="mainContent">
+    <!-- Tabel Konfirmasi Proyek -->
     <div class="card m-5 p-3 border-0 shadow rounded-4" style="width: 61rem;">
       <div class="card-body">
         <div class="table-responsive orderTable">
-        <div class="header d-flex justify-content-between">
-          <h3 class="fw-bold">All Orders</h3>
-          <!-- <input type="text">
+          <div class="header d-flex justify-content-between">
+            <h3 class="fw-bold">All Orders</h3>
+            <!-- <input type="text">
           search -->
 
-        </div>  
-        
-          <!-- <input type="text" class="form-control mb-3" placeholder="Search"> -->
+          </div>
+
+          <table class="table table-borderless">
+            <thead>
+              <tr>
+                <!-- ganti pake looping nanti -->
+                <td class="text-muted text-center">Order ID</td>
+                <td class="text-muted text-center">Project Name</td>
+                <td class="text-muted text-center">Quantity</td>
+                <td class="text-muted text-center">Order Date</td>
+                <td class="text-muted text-center">Est. Date</td>
+                <td class="text-muted text-center">Price</td>
+                <td class="text-muted text-center">Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="text-center">93232</td>
+                <td class="text-center">Pintu Gerbang</td>
+                <td class="text-center">1</td>
+                <td class="text-center">12/12/2024</td>
+                <td class="text-center">1/3/2025</td>
+                <td class="text-center">Rp 120000</td>
+                <td class="text-center">
+                  <p class="detail-btn d-inline"><a class="text-reset text-decoration-none" href="payment.php">Confirm</a></p>
+                </td> <!-- kalo pake button uk nya beda, nnti dicari tau -->
+              </tr>
+              <tr>
+                <td class="text-center">93232</td>
+                <td class="text-center">Pintu Gerbang</td>
+                <td class="text-center">1</td>
+                <td class="text-center">12/12/2024</td>
+                <td class="text-center">1/3/2025</td>
+                <td class="text-center">Rp 120000</td>
+                <td class="text-center">
+                  <p class="detail-btn d-inline"><a class="text-reset text-decoration-none" href="payment.php">Confirm</a></p>
+                </td>
+              </tr>
+          </table>
+          <hr>
+        </div>
+        <div class="d-flex justify-content-between pagination align-items-center">
+          <p class="text-muted orderTable">Showing data 1 to 2 of 256K entries</p>
+          <nav aria-label="Page navigation">
+            <ul class="pagination">
+              <li class="page-item mx-1"><a class="page-link" href="#">&lt;</a></li>
+              <li class="page-item mx-1 active"><a class="page-link" href="#">1</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">2</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">3</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">4</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">&gt;</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tabel Riwayat Proyek -->
+    <div class="card m-5 p-3 border-0 shadow rounded-4" style="width: 61rem;">
+      <div class="card-body">
+        <div class="table-responsive orderTable">
+          <div class="header d-flex justify-content-between">
+            <h3 class="fw-bold">All Orders</h3>
+            <!-- <input type="text">
+          search -->
+
+          </div>
+
           <table class="table table-borderless">
             <thead>
               <tr>
@@ -48,8 +114,11 @@ include __DIR__ . "/templates/header.php";
                 <td class="text-center">1</td>
                 <td class="text-center">12/12/2024</td>
                 <td class="text-center">1/3/2025</td>
-                <td class="text-center"><p class="status-completed d-inline">Completed</p</td>
-                <td class="text-center"><p class="detail-btn d-inline">Detail</p></td>  <!-- kalo pake button uk nya beda, nnti dicari tau -->  
+                <td class="text-center">
+                  <p class="status-completed d-inline">Completed</p< /td>
+                <td class="text-center">
+                  <p class="detail-btn d-inline">Detail</p>
+                </td> <!-- kalo pake button uk nya beda, nnti dicari tau -->
               </tr>
               <tr>
                 <td class="text-center">93232</td>
@@ -57,25 +126,28 @@ include __DIR__ . "/templates/header.php";
                 <td class="text-center">1</td>
                 <td class="text-center">12/12/2024</td>
                 <td class="text-center">1/3/2025</td>
-                <td class="text-center"><p class="status-completed d-inline">Completed</p</td>
-                <td class="text-center"><p class="detail-btn d-inline">Detail</p></td>
+                <td class="text-center">
+                  <p class="status-completed d-inline">Completed</p< /td>
+                <td class="text-center">
+                  <p class="detail-btn d-inline">Detail</p>
+                </td>
               </tr>
           </table>
           <hr>
         </div>
         <div class="d-flex justify-content-between pagination align-items-center">
-            <p class="text-muted orderTable">Showing data 1 to 2 of 256K entries</p>
-            <nav aria-label="Page navigation">
-              <ul class="pagination">
-                <li class="page-item mx-1"><a class="page-link" href="#">&lt;</a></li>
-                <li class="page-item mx-1 active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item mx-1"><a class="page-link" href="#">2</a></li>
-                <li class="page-item mx-1"><a class="page-link" href="#">3</a></li>
-                <li class="page-item mx-1"><a class="page-link" href="#">4</a></li>
-                <li class="page-item mx-1"><a class="page-link" href="#">&gt;</a></li>
-              </ul>
-            </nav>
-          </div>
+          <p class="text-muted orderTable">Showing data 1 to 2 of 256K entries</p>
+          <nav aria-label="Page navigation">
+            <ul class="pagination">
+              <li class="page-item mx-1"><a class="page-link" href="#">&lt;</a></li>
+              <li class="page-item mx-1 active"><a class="page-link" href="#">1</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">2</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">3</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">4</a></li>
+              <li class="page-item mx-1"><a class="page-link" href="#">&gt;</a></li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
 
